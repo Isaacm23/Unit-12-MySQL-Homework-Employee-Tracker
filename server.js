c800d91019bd544e071300ec82f7529ef0789530
Isaac Mckittrick <mckittri.isaa@gmail.com>
2020-06-27 01:33:01 -0700

    added promt

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
+const { isMainThread } = require("worker_threads");
 
 
 var connection = mysql.createConnection({
@@ -10,3 +11,14 @@ var connection = mysql.createConnection({
     password: "Chicken23!",
     database: "employee_tracker"
 });
+connection.connect(err => {if (err) throw err;  start();
+});
+function start() {
+    inquirer
+      .prompt({
+        name: "name",
+        type: "list",
+        message: "Select a menu",
+        choices: ["Departments", "Roles", "Employees"]
+      })
+      
