839f378d8c3038808141c44aa2e218b849d897ac
Isaac Mckittrick <mckittri.isaa@gmail.com>
2020-06-29 00:47:39 -0700

    added different switch case options

--- server.js
+++ server.js
@@ -41,4 +41,40 @@ function start() {
         "exit"
     ];
     
-   
+    start();
+    
+    function start() {
+        inquirer
+            .prompt({
+                name: "action",
+                type: "list",
+                message: "What would you like to do?",
+                choices: viewOptions
+            })
+            .then(function (answer) {
+                switch (answer.action) {
+                    case viewOptions[0]:
+                        departmentView();
+                        break;
+    
+                    case viewOptions[1]:
+                        roleView();
+                        break;
+    
+                    case viewOptions[2]:
+                        employeeView();
+                        break;
+    
+                    case viewOptions[3]:
+                        updateEmployee();
+    
+                    case updateOptions[4]:
+                        connection.end();
+                        break
+                }
+            })
+    }
+    
+    
+    
+    
